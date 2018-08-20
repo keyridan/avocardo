59ecbba2b70728e72a1950430bed8c557fb7deee
keyridan <keyridan@gmail.com>
2018-08-20 18:15:35 +0200

    variant with table

--- src/components/card/FlashCard.js
+++ src/components/card/FlashCard.js
@@ -1,49 +1,92 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import Table from '@material-ui/core/Table'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import withStyles from '@material-ui/core/styles/withStyles'
 import SimpleInput from '../common/SimpleInput'
 import CheckBoxCardItem from '../../containers/CheckBoxCardItem'
 import s from './FlashCard.css'
 import TranslatedTextContainer from '../../containers/TranslatedTextContainer'
 
-const FlashCard = ({ frontSide, backSide, setFrontSide, setBackSideValue }) => (
+const styles = theme => ({
+  card: {
+    ...theme.mixins.gutters(),
+    padding: theme.spacing.unit * 2,
+  },
+})
+
+const FlashCard = ({ frontSide, backSide, setFrontSide, setBackSideValue, classes }) => (
   <div className={s.flash_card} >
-    <Card >
-      <CardContent >
-        <Typography color="textSecondary" >
-          <TranslatedTextContainer value="card_front_side_title" />
-        </Typography >
-        <SimpleInput
-          value={frontSide}
-          onChange={(event) => {
-            setFrontSide(event.target.value)
-          }}
-        />
-      </CardContent >
-    </Card >
-    <Card >
-      <CardContent >
-        <Typography color="textSecondary" >
-          <TranslatedTextContainer value="card_back_side_title" />
-        </Typography >
-        {backSide
-          .map((backSideItem, index) => (
-            <CheckBoxCardItem
-              key={index}
-              checked={parseInt(backSideItem.checked)}
-              index={index}
-            >
-              <SimpleInput
-                key={index}
-                value={backSideItem.value}
-                onChange={event => setBackSideValue(event.target.value, index)}
-              />
-            </CheckBoxCardItem >
-          ))}
-      </CardContent >
-    </Card >
+    <Grid container >
+      <Grid item >
+        <Table >
+          <TableHead >
+            <TableRow >
+              <TableCell >
+                <Typography color="textSecondary" >
+                  <TranslatedTextContainer value="card_front_side_title" />
+                </Typography >
+              </TableCell >
+            </TableRow >
+          </TableHead >
+          <TableBody >
+            <TableRow >
+              <TableCell >
+                <Paper className={classes.card} >
+                  <SimpleInput
+                    value={frontSide}
+                    onChange={(event) => {
+                      setFrontSide(event.target.value)
+                    }}
+                  />
+                </Paper >
+              </TableCell >
+            </TableRow >
+          </TableBody >
+        </Table >
+      </Grid >
+      <Grid item >
+        <Table >
+          <TableHead >
+            <TableRow >
+              <TableCell >
+                <Typography color="textSecondary" >
+                  <TranslatedTextContainer value="card_back_side_title" />
+                </Typography >
+              </TableCell >
+            </TableRow >
+          </TableHead >
+          <TableBody >
+            {backSide
+              .map((backSideItem, index) => (
+                <TableRow >
+                  <TableCell >
+                    <Paper className={classes.card} >
+                      <CheckBoxCardItem
+                        key={index}
+                        checked={parseInt(backSideItem.checked)}
+                        index={index}
+                      >
+                        <SimpleInput
+                          key={index}
+                          value={backSideItem.value}
+                          onChange={event => setBackSideValue(event.target.value, index)}
+                        />
+                      </CheckBoxCardItem >
+                    </Paper >
+                  </TableCell >
+                </TableRow >
+              ))}
+          </TableBody >
+        </Table >
+      </Grid >
+    </Grid >
   </div >
 )
 
@@ -58,4 +101,4 @@ FlashCard.propTypes = {
   reverseCardSides: PropTypes.func.isRequired,
 }
 
-export default FlashCard
+export default withStyles(styles)(FlashCard)
