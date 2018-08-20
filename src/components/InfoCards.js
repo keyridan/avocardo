6685e37e69b0aeb21777611caa684caa6d2635ef
keyridan <keyridan@gmail.com>
2018-08-20 12:46:35 +0200

    updated proptypes

--- src/components/InfoCards.js
+++ src/components/InfoCards.js
@@ -12,24 +12,30 @@ const styles = theme => ({
   },
 })
 
-const InfoCards = ({ infos, className, infoProvider, classes }) => (
+const InfoCards = ({
+  infos, className, infoProvider, classes,
+}) => (
   <div className={`${className} ${classes.container}`} >
-    <Typography variant="title" gutterBottom>
+    <Typography variant="title" gutterBottom >
       <TranslatedTextContainer value="additional_info" />
-    </Typography>
-    {INFO_PROVIDERS.map((provider, index) =>
-      (<InfoPanel
-          info={infos[provider]}
-          type={provider}
-          key={index}
-          checked={infoProvider[provider].checked}
-        />
-      ))}
+    </Typography >
+    {INFO_PROVIDERS.map((provider, index) => (
+      <InfoPanel
+        info={infos[provider]}
+        type={provider}
+        key={index}
+        checked={infoProvider[provider].checked}
+      />
+    ))}
   </div >
 )
 
 InfoCards.propTypes = {
+  infos: PropTypes.arrayOf(PropTypes.shape({
+    info: PropTypes.object,
+  })).isRequired,
   className: PropTypes.string.isRequired,
+  infoProvider: PropTypes.object.isRequired,
 }
 
 export default withStyles(styles)(InfoCards)
